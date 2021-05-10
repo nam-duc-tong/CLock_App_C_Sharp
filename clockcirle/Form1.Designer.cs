e77e5b67f03041245f532ec41c6e3ad3270a7078
Tống Đức Nam <637849@sv.vnua.edu.vn>
2021-05-10 21:15:20 +0700

    first commit

---
+++ clockcirle/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace clockcirle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.lbSecond = new System.Windows.Forms.Label();
+            this.lbDay = new System.Windows.Forms.Label();
+            this.lbDate = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbTime.Font = new System.Drawing.Font("Bernard MT Condensed", 80.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTime.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbTime.Location = new System.Drawing.Point(151, 52);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(292, 129);
+            this.lbTime.TabIndex = 0;
+            this.lbTime.Text = "22:22";
+            // 
+            // lbSecond
+            // 
+            this.lbSecond.AutoSize = true;
+            this.lbSecond.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbSecond.Font = new System.Drawing.Font("Bernard MT Condensed", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSecond.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbSecond.Location = new System.Drawing.Point(463, 157);
+            this.lbSecond.Name = "lbSecond";
+            this.lbSecond.Size = new System.Drawing.Size(51, 40);
+            this.lbSecond.TabIndex = 1;
+            this.lbSecond.Text = "22";
+            // 
+            // lbDay
+            // 
+            this.lbDay.AutoSize = true;
+            this.lbDay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbDay.Font = new System.Drawing.Font("Bernard MT Condensed", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDay.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbDay.Location = new System.Drawing.Point(373, 212);
+            this.lbDay.Name = "lbDay";
+            this.lbDay.Size = new System.Drawing.Size(169, 59);
+            this.lbDay.TabIndex = 3;
+            this.lbDay.Text = "Monday";
+            // 
+            // lbDate
+            // 
+            this.lbDate.AutoSize = true;
+            this.lbDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbDate.Font = new System.Drawing.Font("Bernard MT Condensed", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbDate.Location = new System.Drawing.Point(54, 212);
+            this.lbDate.Name = "lbDate";
+            this.lbDate.Size = new System.Drawing.Size(254, 59);
+            this.lbDate.TabIndex = 2;
+            this.lbDate.Text = "May 10 2021";
+            this.lbDate.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(467, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(557, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(665, 318);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbDay);
+            this.Controls.Add(this.lbDate);
+            this.Controls.Add(this.lbSecond);
+            this.Controls.Add(this.lbTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Label lbSecond;
+        private System.Windows.Forms.Label lbDay;
+        private System.Windows.Forms.Label lbDate;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
---
+++ clockcirle/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clockcirle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbTime.Text = DateTime.Now.ToString("HH:mm");
+            lbSecond.Text = DateTime.Now.ToString("ss");
+            lbDate.Text = DateTime.Now.ToString("MM dd yyyy");
+            lbDay.Text = DateTime.Now.ToString("dddd");
+            lbSecond.Location = new Point(lbTime.Location.X + lbTime.Width, lbSecond.Location.Y);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
